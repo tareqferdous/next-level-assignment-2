faa121dca9ba625cdf48907d7a7cb1e7852053da
tareqferdous <16101017@uap-bd.edu>
2025-12-08 10:05:00 +0600

    role base access added vor vehicles

--- src/modules/vechiles/vehicles.route.ts
+++ src/modules/vechiles/vehicles.route.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
+import auth from "../../middleware/auth";
+import { Roles } from "../auth/auth.constant";
 import { vehiclesController } from "./vehicles.controller";
 
 const router = Router();
 
-router.post("/", vehiclesController.createVehicle);
+router.post("/", auth(Roles.admin), vehiclesController.createVehicle);
 router.get("/", vehiclesController.getAllVehicles);
 router.get("/:vehicleId", vehiclesController.getVehicleDetails);
-router.put("/:vehicleId", vehiclesController.updateVehicleDetails);
-router.delete("/:vehicleId", vehiclesController.deleteVehicle);
+router.put(
+  "/:vehicleId",
+  auth(Roles.admin),
+  vehiclesController.updateVehicleDetails
+);
+router.delete(
+  "/:vehicleId",
+  auth(Roles.admin),
+  vehiclesController.deleteVehicle
+);
 
 export const vehiclesRoute = router;
