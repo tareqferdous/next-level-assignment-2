ff6365121b65694b28d55a2b2ae711e32500206d
tareqferdous <16101017@uap-bd.edu>
2025-12-24 16:27:10 +0600

    update config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
-import path from "path";
 
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+dotenv.config();
 
 const config = {
   connection_str: process.env.CONNECTION_STR,
