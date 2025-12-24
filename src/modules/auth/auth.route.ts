6e6663dbec6438b2c3e1c1c34f83591e013be726
tareqferdous <16101017@uap-bd.edu>
2025-12-24 14:55:48 +0600

    auth route updated

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -2,6 +2,6 @@ import { Router } from "express";
 import { authController } from "./auth.controller";
 
 const router = Router();
-router.post("/login", authController.loginUser);
+router.post("/signin", authController.loginUser);
 router.post("/signup", authController.registerUser);
 export const authRoute = router;
